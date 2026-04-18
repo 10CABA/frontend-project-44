583f236752c46bc37cc04ecbe4fc882c65281adf
10CABA <savakochkin07@yandex.ru>
2026-04-18 14:07:20 +0300

    zadanie_7

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/10CABA/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/10CABA/frontend-project-44/actions)
 
+## Проверка на чётность
+
 [![asciicast](https://asciinema.org/a/oI69rmhim3XRBkiY)](https://asciinema.org/a/oI69rmhim3XRBkiY)
+
+## Калькулятор
+
+[![asciicast](https://asciinema.org/a/ССЫЛКА_НА_ЗАПИСЬ_ДЛЯ_CALC)](https://asciinema.org/a/ССЫЛКА_НА_ЗАПИСЬ_ДЛЯ_CALC)
+
+## НОД
+
+[![asciicast](https://asciinema.org/a/ССЫЛКА_НА_НОВУЮ_ЗАПИСЬ)](https://asciinema.org/a/ССЫЛКА_НА_НОВУЮ_ЗАПИСЬ)
---
+++ src/games/gcd.js
@@ -0,0 +1,32 @@
+import getRandomNumber from '../utils.js';
+
+function findGCD(a, b) {
+  let num1 = a
+  let num2 = b
+  
+  while (num2 !== 0) {
+    const temp = num2
+    num2 = num1 % num2
+    num1 = temp
+  }
+  
+  return num1
+}
+
+function generateRound() {
+  const num1 = getRandomNumber(1, 100)
+  const num2 = getRandomNumber(1, 100)
+  
+  const question = num1 + ' ' + num2
+  const correctAnswer = String(findGCD(num1, num2))
+  
+  return {
+    question: question,
+    correctAnswer: correctAnswer,
+  }
+}
+
+export default {
+  rules: 'Find the greatest common divisor of given numbers.',
+  generateRound: generateRound,
+};
