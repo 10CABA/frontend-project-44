acae354d9d48b1ca42e19f4bf64bc3449a0c45f6
10CABA <savakochkin07@yandex.ru>
2026-04-18 15:09:08 +0300

    zadanie_9_last_commit?

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ function isEven(num) {return num % 2 === 0}
 
 function generateRound() {
   const randomNumber = getRandomNumber(1, 100)
-  let correctAnswer = isEven(randomNumber) ? yes : no
+  let correctAnswer = isEven(randomNumber) ? 'yes' : 'no'
   
   return {
     question: String(randomNumber),
