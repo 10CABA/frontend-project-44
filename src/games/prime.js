12d5e7353ee9c0a359fda0772ff636da31d14707
10CABA <savakochkin07@yandex.ru>
2026-04-18 14:35:01 +0300

    zadanie_9_err1

---
+++ src/games/prime.js
@@ -0,0 +1,32 @@
+import getRandomNumber from '../utils.js';
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+function generateRound() {
+  const randomNumber = getRandomNumber(1, 100);
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+  
+  return {
+    question: String(randomNumber),
+    correctAnswer: correctAnswer,
+  };
+}
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+export default {
+  rules: gameRules,
+  generateRound: generateRound,
+};
